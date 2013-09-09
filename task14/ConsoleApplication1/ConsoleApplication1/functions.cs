ef4cb63253b1df753abbcef27e5a224a29496eb1
mikkiemouse <mikhail.strokov@gmail.com>
2013-09-10 00:29:23 +0400

    test14

---
+++ task14/ConsoleApplication1/ConsoleApplication1/functions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class functions
+    {
+         public static int factorial(int a)
+        {
+            int i;
+            int s = 1;
+            for (i = 1; i <= a; i++)
+            {
+                s = s * i;
+            }
+            return s;
+    }
+}
+}
