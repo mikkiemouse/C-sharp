3bf76838847c7ebb74bb59ff03fa8ff924fb8246
mikkiemouse <mikhail.strokov@gmail.com>
2013-09-10 09:21:50 +0400

    conditional opetator(task14)

---
+++ conditional operators(task14)/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            double  x, sum;
+            sum = 1;
+            n = InputValue("N");
+            x = InputValue("x");
+            for (i = 2; i <= 2 * n; i += 2)
+            {
+                sum += Math.Pow(x, n) / functions.factorial(n);
+            }
+            Console.WriteLine("The amount of N elements {0}", sum);
+        }
+
+        private static int InputValue(string name)
+        {
+            int n;
+            Console.WriteLine("Enter {0}", name);
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out n))
+                {
+                    return n;
+                }
+                Console.WriteLine("Incorrect vlue, please try again");
+            }
+        }
+    }
+}
